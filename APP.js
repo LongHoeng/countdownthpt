359910e9f33c8f0e3fc02f40d17e9f4b93d7d99c
LongHoeng <86183209+LongHoeng@users.noreply.github.com>
2022-08-29 23:30:50 +0700

    d

---
+++ APP.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import Confetti from "react-confetti";
+import Fireworks from "react-native-fireworks";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Confetti width="2000" height="2000" />
+    </div>
+  );
+}
